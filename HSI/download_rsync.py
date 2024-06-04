05417f4896ade2b1d235aa51dda8b2d24b74bfe3
mmckay18 <mckaymyle@gmail.com>
2024-06-04 12:32:27 -0700

    chore: Update file paths for downloading MaNGA DAP Maps

--- HSI/download_rsync.py
+++ HSI/download_rsync.py
@@ -4,24 +4,29 @@ import os
 import pandas as pd
 
 
-pw_file = "/Users/mmckay/Desktop/sdss_pw.txt"  # SDSS Collab password file
+pw_file = "/Users/mmckay/Desktop/gumby-spectral-imaging_fork/HSI/sdss_manga_dapall_data/sdss_pw.txt"  # SDSS Collab password file
 
 # Downloading data DAP Maps and Pipe3D Maps from MaNGA MPL11
 
 # DAP Maps for BBRD sample
 # Read in csv with BreakBRD plateifu
-bbrd_table = pd.read_csv(
-    "/Users/mmckay/Desktop/research/FMR_MZR/final_MaNGAdr16_bbrd_crossmatch.csv"
+df = pd.read_csv(
+    "/Users/mmckay/Desktop/gumby-spectral-imaging_fork/HSI/sdss_manga_dapall_data/drpall_random_subsample.csv"
 )
 
 # Interate through the plateifu and download MPL11 DAP Maps
-plateifu_list = bbrd_table["plateifu"]
+plateifu_list = df["plateifu"]
+key = 'LOGCUBE' # 'LOGCUBE' or 'MAPS'
 for plateifu in plateifu_list:
     plate, ifu = plateifu.split("-")
 
     # Retrieves DAP Maps
+    # os.system(
+    #     "rsync -avz --password-file {} rsync://sdss@dtn01.sdss.utah.edu/sas/mangawork/manga/spectro/analysis/MPL-11/HYB10-MILESHC-MASTARSSP/{}/{}/*manga-{}-{}-MAPS-HYB10-MILESHC-MASTARSSP.fits.gz /Volumes/lil_oynx/bbrd_dapfits".format(
+    #         pw_file, plate, ifu, plate, ifu
+    #     )
+    # )
+
     os.system(
-        "rsync -avz --password-file {} rsync://sdss@dtn01.sdss.utah.edu/sas/mangawork/manga/spectro/analysis/MPL-11/HYB10-MILESHC-MASTARSSP/{}/{}/*manga-{}-{}-MAPS-HYB10-MILESHC-MASTARSSP.fits.gz /Volumes/lil_oynx/bbrd_dapfits".format(
-            pw_file, plate, ifu, plate, ifu
-        )
+        f"rsync -avz --password-file {pw_file} rsync://sdss@dtn01.sdss.utah.edu/dr17/manga/spectro/analysis/v3_1_1/3.1.0/SPX-MILESHC-MASTARSSP/{plate}/{ifu}/*manga-{plate}-{ifu}-{key}-SPX-MILESHC-MASTARSSP.fits.gz /gscratch/astro/mmckay18/DATA/raw/{plate}/{ifu}/"
     )
