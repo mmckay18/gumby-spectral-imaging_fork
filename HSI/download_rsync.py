8ac0be19a5dc3b5672281d04eace9a97702434ab
mmckay18 <mckaymyle@gmail.com>
2024-06-08 14:48:35 -0700

    Refactor download_rsync.py to remove empty directories after downloading MaNGA DAP Maps

--- HSI/download_rsync.py
+++ HSI/download_rsync.py
@@ -10,30 +10,40 @@ import pandas as pd
 # from download_utils import download_single_cube
 from download_utils import *
 
+def remove_empty_dirs(path):
+    # Walk through the directory tree, from bottom to top
+    for root, dirs, files in os.walk(path, topdown=False):
+        for dir in dirs:
+            dir_path = os.path.join(root, dir)
+            # Check if the directory is empty
+            if not os.listdir(dir_path):
+                # Remove the empty directory
+                os.rmdir(dir_path)
+                print(f"Removed empty directory: {dir_path}")
 
-pw_file = "/gscratch/astro/mmckay18/gumby-spectral-imaging_fork/HSI/sdss_manga_dapall_data/sdss_pw.txt"  # SDSS Collab password file
 
-# Downloading data DAP Maps and Pipe3D Maps from MaNGA MPL11
+# # SDSS Collab password file
+# pw_file = "/gscratch/astro/mmckay18/gumby-spectral-imaging_fork/HSI/sdss_manga_dapall_data/sdss_pw.txt"  
 
-# DAP Maps for BBRD sample
-# Read in csv with BreakBRD plateifu
-df = pd.read_csv(
-    "/gscratch/astro/mmckay18/gumby-spectral-imaging_fork/HSI/sdss_manga_dapall_data/drpall_random_subsample.csv"
-)
+# # Downloading data DAP Maps and Pipe3D Maps from MaNGA MPL11
 
-# Interate through the plateifu and download MPL11 DAP Maps
-plateifu_list = df["plateifu"].to_list()
-# print(plateifu_list[:5])
-key = 'LOGCUBE' # 'LOGCUBE' or 'MAPS'
-for plateifu in plateifu_list[:800]:
-    plateifu = plateifu.strip(" \t")
-    plate, ifu = plateifu.split("-")[0].replace(" ", ""), plateifu.split("-")[1].replace(" ", "")
-    # print(plate, ifu)
-    file_url, save_path = get_url(plateifu, key=key)
-    print(file_url)
-    print(save_path)
-    results = fetch_and_save(file_url, save_path)
-    print(results)
+# # DAP Maps for BBRD sample
+# # Read in csv with BreakBRD plateifu
+# df = pd.read_csv(
+#     "/gscratch/astro/mmckay18/gumby-spectral-imaging_fork/HSI/sdss_manga_dapall_data/drpall_random_subsample.csv"
+# )
+
+# # Interate through the plateifu and download MPL11 DAP Maps
+# plateifu_list = df["plateifu"].to_list()
+# # print(plateifu_list[:5])
+# key = 'LOGCUBE' # 'LOGCUBE' or 'MAPS'
+# for plateifu in plateifu_list[:800]:
+#     plateifu = plateifu.strip(" \t")
+#     plate, ifu = plateifu.split("-")[0].replace(" ", ""), plateifu.split("-")[1].replace(" ", "")
+#     file_url, save_path = get_url(plateifu, key=key)
+#     # print(file_url)
+#     # print(save_path)
+#     results = fetch_and_save(file_url, save_path)
 
     # # Make directory for cube and maps
     # print('Make directory for cube and maps')
@@ -49,6 +59,10 @@ for plateifu in plateifu_list[:800]:
 
 
 # fits_file = '/gscratch/astro/mmckay18/DATA/raw/10001/12701/manga-10001-12701-LOGCUBE-SPX-MILESHC-MASTARSSP.fits.gz'
-    fits_file = f'manga-{plate}-{ifu}-{key}-SPX-MILESHC-MASTARSSP.fits.gz'
-    print(fits_file)
-    download_single_cube(fits_file, key='LOGCUBE')
+    # fits_file = f'manga-{plate}-{ifu}-{key}-SPX-MILESHC-MASTARSSP.fits.gz'
+    # print(fits_file)
+    # download_single_cube(fits_file, key='LOGCUBE')
+
+
+# Clean directory tree
+remove_empty_dirs(path_to_clean = "/gscratch/astro/mmckay18/DATA/raw/")
