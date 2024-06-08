85575963f63a7967f4b946f1dca300b92023ad0a
mmckay18 <mckaymyle@gmail.com>
2024-06-08 11:19:39 -0700

    Refactor download_rsync.py to use dynamic key for fits file name

--- HSI/download_rsync.py
+++ HSI/download_rsync.py
@@ -23,9 +23,9 @@ df = pd.read_csv(
 
 # Interate through the plateifu and download MPL11 DAP Maps
 plateifu_list = df["plateifu"].to_list()
-print(plateifu_list[:800])
+# print(plateifu_list[:5])
 key = 'LOGCUBE' # 'LOGCUBE' or 'MAPS'
-for plateifu in plateifu_list[:5]:
+for plateifu in plateifu_list[:800]:
     plateifu = plateifu.strip(" \t")
     plate, ifu = plateifu.split("-")[0].replace(" ", ""), plateifu.split("-")[1].replace(" ", "")
     # print(plate, ifu)
@@ -45,7 +45,7 @@ for plateifu in plateifu_list[:5]:
     # os.system(f"rsync -avz --password-file {pw_file} rsync://sdss@dtn01.sdss.utah.edu/dr17/manga/spectro/analysis/v3_1_1/3.1.0/SPX-MILESHC-MASTARSSP/{plate}/{ifu}/manga-{plate}-{ifu}-{key}-SPX-MILESHC-MASTARSSP.fits.gz /gscratch/astro/mmckay18/DATA/raw/{plate}/{ifu}/")
     
     # Retrieves DAP Maps
-    os.system(f"rsync -avz --password-file {pw_file} --no-motd rsync://data.sdss.org/dr17/manga/spectro/analysis/v3_1_1/3.1.0/SPX-MILESHC-MASTARSSP/{plate}/{ifu}/manga-{plate}-{ifu}-{key}-SPX-MILESHC-MASTARSSP.fits.gz /gscratch/astro/mmckay18/DATA/raw/{plate}/{ifu}/")
+    os.system(f"rsync -avz --password-file {pw_file} --no-motd rsync://data.sdss.org/sas/dr17/manga/spectro/analysis/v3_1_1/3.1.0/SPX-MILESHC-MASTARSSP/{plate}/{ifu}/manga-{plate}-{ifu}-{key}-SPX-MILESHC-MASTARSSP.fits.gz /gscratch/astro/mmckay18/DATA/raw/{plate}/{ifu}/")
 
 #     # https://data.sdss.org/sas/dr17/manga/spectro/analysis/v3_1_1/3.1.0/SPX-MILESHC-MASTARSSP/10001/12701/manga-10001-12701-LOGCUBE-SPX-MILESHC-MASTARSSP.fits.gz
 
